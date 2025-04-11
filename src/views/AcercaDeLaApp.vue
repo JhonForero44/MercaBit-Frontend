396d202b4baf35839ed07069cd3587c7c06e25a0
JhonForero44 <jhonforero44@gmail.com>
2025-04-11 12:04:35 -0500

    Pagina Base Acerca de la App

---
+++ src/views/AcercaDeLaApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <main class="content">
+      <h1>Bienvenido a Accerca de la App</h1>
+    </main>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.page {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+</style>
