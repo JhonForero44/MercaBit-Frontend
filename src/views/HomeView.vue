50108f9bb3302d87dc539f5553a010d85d8a9d77
JhonForero44 <jhonforero44@gmail.com>
2025-04-11 12:03:38 -0500

    Base pagina Home

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="page">
+      <main class="content">
+        <h1>Bienvenido a la App</h1>
+      </main>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .page {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  </style>
+  
