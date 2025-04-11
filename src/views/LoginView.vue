204c0623e9b6bb4efcb8a278e98df124ec38fe63
JhonForero44 <jhonforero44@gmail.com>
2025-04-11 12:06:23 -0500

    Actualizacion vista Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,14 +5,20 @@
         <div class="header">
           <h1>Inicio de Sesión</h1>
         </div>
-        <input v-model="email" class="custom-input" placeholder="Correo electrónico" type="email" />
-        <input v-model="password" class="custom-input" placeholder="Ingresa tu contraseña" type="password" />
-        <button class="DesignButton-Ingresar" @click="login">Ingresar</button>
+
+        <CustomInput v-model="email" placeholder="Correo electrónico" type="email" />
+        <CustomInput v-model="password" placeholder="Ingresa tu contraseña" type="password" />
+        <DesignButton :label="'Ingresar'" :onClick="goToIngresar" class="DesignButton-Ingresar">
+          <span v-if="!loading">Ingresar</span>
+          <span v-else>Cargando...</span>
+        </DesignButton>
+
         <p class="link" @click="goToRecuperarContraseña">Olvidé mi contraseña</p>
         <p class="register-text">¿No tienes una cuenta?</p>
-        <button class="DesignButton-Register" @click="goToRegister">
-          Crea una Cuenta
-        </button>
+        <DesignButton :label="'Crea una Cuenta'" :onClick="goToRegister" class="DesignButton-Register">
+          <span v-if="!loading">Crear cuenta</span>
+          <span v-else>Cargando...</span>
+        </DesignButton>
 
         <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
       </div>
@@ -21,21 +27,34 @@
 </template>
 
 <script setup>
+import CustomInput from '@/components/CustomInput.vue'
+import DesignButton from '@/components/DesignButton.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToRegister = () => {
+  router.push('/registro')
+}
+
+const goToRecuperarContraseña = () => {
+  router.push('/recuperar-contrasena')
+}
 
 </script>
 
 <style scoped>
+
+.header {
+  text-align: center;
+  font-size: 15px;
+}
+
 .login-page {
   height: 80vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-}
-
-
-.header {
-  text-align: center;
-  font-size: 15px;
 }
 
 .logo {
@@ -80,23 +99,20 @@
   color: black;
 }
 
-button {
-  width: 86%;
-  height: 50px;
-  font-size: 16px;
-  border-radius: 20px;
-  border: none;
-  color: white;
-  margin: 10px auto;
-  cursor: pointer;
-}
-
 .DesignButton-Ingresar {
   background-color: #258D60;
 }
 
+.DesignButton-Ingresar:hover {
+  background-color: #30a773;
+}
+
 .DesignButton-Register {
   background-color: #252E8D;
+}
+
+.DesignButton-Register:hover {
+  background-color: #0056b3;
 }
 
 .link {
