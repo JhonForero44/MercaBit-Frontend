13c022d7aebcc2f08a2d8c02843b1b279e6aba0b
JhonForero44 <jhonforero44@gmail.com>
2025-04-15 11:50:14 -0500

    Importar Iconos

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'// import Router from './router'
 import './assets/styles.css';
+import '@fortawesome/fontawesome-free/css/all.css'; // Import Font Awesome CSS
 
 const app = createApp(App)
 app.use(router)
