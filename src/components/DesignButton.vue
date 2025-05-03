a719daf72e0f83d4d2386242be371be5fa129982
Jhon Forero <jhonforero44@gmail.com>
2025-05-03 11:15:27 -0500

    Solucion problema de librerias

--- src/components/DesignButton.vue
+++ src/components/DesignButton.vue
@@ -1,17 +1,21 @@
 <template>
-    <button class="design-button" @click="onClick">
-        {{ label }}
-        <slot />
-    </button>
+  <button class="design-button" @click="onClick" :disabled="disabled">
+    {{ label }}
+    <slot />
+  </button>
 </template>
 
 <script>
 export default {
   name: 'DesignButton',
   props: {
+    label: {
+      type: String,
+      required: true
+    },
     onClick: {
       type: Function,
-      default: () => {}
+      default: () => { }
     },
     disabled: {
       type: Boolean,
@@ -23,17 +27,17 @@ export default {
 
 <style scoped>
 .design-button {
-    width: 86%;
-    height: 50px;
-    font-size: 16px;
-    border-radius: 20px;
-    border: none;
-    color: white;
-    margin: 10px auto;
-    cursor: pointer;
-    display: block;
-    text-align: center;
-    transition: background-color 0.3s;
-    font-weight: bold;
+  width: 86%;
+  height: 50px;
+  font-size: 16px;
+  border-radius: 20px;
+  border: none;
+  color: white;
+  margin: 10px auto;
+  cursor: pointer;
+  display: block;
+  text-align: center;
+  transition: background-color 0.3s;
+  font-weight: bold;
 }
 </style>
